32e0f11a61361aed9c2cad754909e3304edd7245
Reginaldo-Neto <reginaldoneto09@gmail.com>
2024-12-21 21:58:19 +0000

    inserts

--- honeypot/home/honeypot/docker/mysql-init/01-insert_users.sql
+++ honeypot/home/honeypot/docker/mysql-init/01-insert_users.sql
@@ -1,6 +1,6 @@
 use secure_db;
 
-INSERT INTO users (username, password, is_admin, full_name) VALUES ('oak', '$2y$10$mT4HObS63D1uwyxIpq8h1eZ/U1oD3rNZKK2bScT5.MNYbkHIGHvPW', TRUE, 'Professor Oak');
-INSERT INTO users (username, password, is_admin, full_name) VALUES ('ash', '$2y$10$eccxHA0E./SgklCgIlxeROpQmqsIsNlTWWNvyeYaYAeAnCb0i0b4K', FALSE, 'Ash Ketchum');
-INSERT INTO users (username, password, is_admin, full_name) VALUES ('misty', '$2y$10$X0.8./A5RKP2mo96FSH14umw335hL4JfwYIwXJH1Dd4rxUrOLN8Rq', FALSE, 'Misty Kasumi');
-INSERT INTO users (username, password, is_admin, full_name) VALUES ('jessie', '$2y$10$JS9n9Spq7VjQxE1fEuD1zuYk/E7oKECy0Cd.CqGgaqkyPyxpKzoOG', FALSE, 'Jessie Rocket');
+INSERT INTO users (username, password, is_admin, full_name) VALUES ('pedrosa', 'pedrosa123', TRUE, 'Professor Pedrosa');
+INSERT INTO users (username, password, is_admin, full_name) VALUES ('rift', 'rift123', FALSE, 'Rift ');
+INSERT INTO users (username, password, is_admin, full_name) VALUES ('man', 'man123', FALSE, 'Man');
+INSERT INTO users (username, password, is_admin, full_name) VALUES ('thiago', 'thiago123', FALSE, 'Thiago');
--- honeypot/home/honeypot/docker/mysql-init/03-insert_users_pokemon.sql
+++ honeypot/home/honeypot/docker/mysql-init/03-insert_users_pokemon.sql
@@ -1,19 +1,19 @@
 use secure_db;
 
--- ash
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 1);
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 6);
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 7);
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 12);
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 17);
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 25);
+-- 
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 1001);
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 1006);
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 1007);
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 1012);
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 1017);
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (2, 1025);
 
--- misty
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (3, 54);
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (3, 118);
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (3, 120);
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (3, 121);
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (3, 175);
+-- 
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (3, 1004);
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (3, 1018);
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (3, 1020);
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (3, 1021);
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (3, 1005);
 
--- jessy
-INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (4, 52);
+-- 
+INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (4, 1002);
